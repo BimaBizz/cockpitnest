0a468206fa674fa25c12a716d8fc9bfea2b44efe
Bima Mahendra <mahendrabima278@gmail.com>
2024-12-05 11:33:45 +0700

    add sample database cockpit cms

---
+++ public/DataBase/storage/detektivo/products.index.php
@@ -0,0 +1,19 @@
+<?php
+ return [
+  'name' => 'products',
+  'label' => '',
+  'info' => '',
+  'type' => 'content:collection',
+  'fields' => [
+    0 => 'title',
+    1 => 'description',
+    2 => 'slug',
+  ],
+  'group' => NULL,
+  'meta' => [
+    'model' => 'products',
+    'populate' => 1,
+    'locale' => false,
+  ],
+  '_created' => 1733291171,
+];
