8878ae5131364f9e6fe9817914e93eb0c7ad889a
Bima Mahendra <mahendrabima278@gmail.com>
2025-01-26 00:58:28 +0700

    refactor: remove unused globals.css and update import paths in layout.js and not-found.js

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -1,4 +1,4 @@
-import "./globals.css";
+import "./../globals.css";
 import { Poppins } from 'next/font/google';
 import { fetchSettings } from '@/lib/hook';
 import { cookies } from 'next/headers'; // Import cookies
--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,21 +1,20 @@
-'use client';
- 
+"use client";
+
 import "./globals.css";
 import Error from "next/error";
 
- 
 export default function NotFound() {
   return (
     <html lang="en">
       <body className="bg-base-100">
         <div className="mockup-browser border-base-300 border h-screen">
-      <div className="mockup-browser-toolbar">
-        <div className="input border-base-300 border"></div>
-      </div>
-      <div className="border-base-300 flex justify-center items-center border-t h-full">
-      <Error statusCode={404} />
-      </div>
-    </div>
+          <div className="mockup-browser-toolbar">
+            <div className="input border-base-300 border"></div>
+          </div>
+          <div className="border-base-300 flex justify-center items-center border-t h-full">
+            <Error statusCode={404} />
+          </div>
+        </div>
       </body>
     </html>
   );
